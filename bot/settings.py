db5c0b533fa93812a8296a90b6441cffe29edbd8
Pavel Komarov <72350156+pkomarov53@users.noreply.github.com>
2026-01-21 16:12:44 +0300

    Update settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -1,5 +1,7 @@
-TOKEN = "7698217701:AAEEYu7_l-nRfX7pL3gne_U7SW9y--7OXVI"
-ADMIN_ID = 473516172  # Замените на свой ID (можно узнать у @userinfobot)
+TOKEN = ""
+ADMIN_ID =   #  @userinfobot
+
 TEMPLATE_PATH = "templates/child-sert.png"
 FONT_PATH = "templates/default.ttf"
+
 FONT_SIZE = 60
